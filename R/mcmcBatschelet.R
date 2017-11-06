7c34eeb0d9cf90d970c44f65545a193228fe1e54
Kees Mulder <keestimmulder@gmail.com>
2017-11-06 13:12:07 +0100

    Fixed bug in W matrix computation

--- R/mcmcBatschelet.R
+++ R/mcmcBatschelet.R
@@ -5,6 +5,11 @@ ll_rhs_bat <- function(x, mu, kp, lam, tlam_fun) {
 }
 
 
+# The left hand side of the log likelihood of a Batschelet-type distribution
+ll_lhs_bat <- function(x, mu, kp, lam, tlam_fun) {
+  kp * sum(cos(tlam_fun(x - mu, lam)))
+}
+
 
 sample_mu_batmix <- function(x, mu_cur, kp, lam, tlam_fun) {
 
@@ -19,7 +24,6 @@ sample_mu_batmix <- function(x, mu_cur, kp, lam, tlam_fun) {
   ll_can <- ll_rhs_bat(x, mu_can, kp, lam, tlam_fun)
   ll_cur <- ll_rhs_bat(x, mu_cur, kp, lam, tlam_fun)
 }
-
 
 
 
@@ -44,6 +48,10 @@ mcmcBatscheletMixture <- function(x, Q = 1000,
     stop("Unknown Batschelet type.")
   }
 
+  # A matrix with logicals for each initial value of the parameter matrix.
+  na_fixedpmat <- is.na(fixed_pmat)
+  na_initpmat  <- is.na(init_pmat)
+
   # Set initial values if the initial parameter matrix is not given for that parameter (has NAs).
   if (any(na_initpmat[, 1])) init_pmat[, 1] <- seq(0, 2*pi, length.out = n_comp + 1)[-1]
   if (any(na_initpmat[, 2])) init_pmat[, 2] <- rep(5, n_comp)
@@ -53,9 +61,6 @@ mcmcBatscheletMixture <- function(x, Q = 1000,
   # Force x to be in range -pi, pi.
   x <- force_neg_pi_pi(x)
 
-  # A matrix with logicals for each initial value of the parameter matrix.
-  na_fixedpmat <- is.na(fixed_pmat)
-  na_initpmat  <- is.na(init_pmat)
 
   n <- length(x)
 
@@ -69,11 +74,10 @@ mcmcBatscheletMixture <- function(x, Q = 1000,
   z_cur <- integer(n)
 
   # initialize W matrix.
-  W <- matrix(1/n_comp, nrow = n, ncol = n_comp)
+  W <- matrix(alph_cur, nrow = n, ncol = n_comp, byrow = TRUE)
 
 
   for (i in 2:Q) {
-
 
     ### Sample group assignments z
 
@@ -89,8 +93,8 @@ mcmcBatscheletMixture <- function(x, Q = 1000,
     z_cur <- apply(W, 1, function(row_probs) sample(x = 1:n_comp, size = 1, prob = row_probs))
 
     # Sample weights alph
-    dir_alpha <- sapply(1:n_comp, function(j) sum(z_cur == j))
-    alph_cur  <- ifelse(na_fixedpmat[, 4], MCMCpack::rdirichlet(1, dir_alpha + 1), alph_cur)
+    dir_asum <- sapply(1:n_comp, function(j) sum(z_cur == j))
+    alph_cur <- ifelse(na_fixedpmat[, 4], MCMCpack::rdirichlet(1, dir_asum + 1), alph_cur)
 
     # After sampling the current group assignments, the parameters for each
     # component only can be sampled separately.
@@ -98,8 +102,6 @@ mcmcBatscheletMixture <- function(x, Q = 1000,
 
       # Dataset assigned to this component.
       x_j <- x[z_cur == j]
-
-
 
       # Sample mu
       mu_cur    <- sample_mu_bat(x_j, mu_cur[j], kp_cur[j], lam_cur[j], tlam_fun)
