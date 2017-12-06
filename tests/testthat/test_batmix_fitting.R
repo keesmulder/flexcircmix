7c8d62fbffcf321a9e5c189f310517fde44cf252
Kees Mulder <keestimmulder@gmail.com>
2017-12-06 19:51:45 +0100

    Additional tests for fitbatmix

--- tests/testthat/test_batmix_fitting.R
+++ tests/testthat/test_batmix_fitting.R
@@ -16,7 +16,35 @@ test_that("EM fitbatmix wrapper works", {
   expect_true(fit_inv$method == "EM")
   expect_true(fit_pow$method == "EM")
 
+
+  # Works with a single component
+  expect_error(
+    fit_pow <- fitbatmix(x, n_comp = 1, verbose = TRUE,
+                         bat_type = "power", method = "EM")
+    , NA)
+
+
+
 })
+
+test_that("Fixing parameters works", {
+
+  x <- rinvbatmix(50)
+
+  fixed_pmat = matrix(c(NA, NA, 3, NA,
+                        1, NA, NA, 3.14,
+                        NA, 2, NA, NA),
+                      nrow = 3, ncol = 4, byrow = TRUE)
+
+  fit_pow <- fitbatmix(x, n_comp = 1, verbose = TRUE,
+                       bat_type = "power", method = "EM")
+
+
+  expect_true(fit_inv$method == "EM")
+  expect_true(fit_pow$method == "EM")
+
+})
+
 
 test_that("Bootstrap works", {
 
