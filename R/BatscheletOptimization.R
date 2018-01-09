ccc807154a49e3911793b1aa033f9ee064ee3ee5
Kees Mulder <keestimmulder@gmail.com>
2018-01-09 10:27:57 +0100

    Explicitly link to stats

--- R/BatscheletOptimization.R
+++ R/BatscheletOptimization.R
@@ -43,13 +43,13 @@ maxlikbat <- function(x, likfunbat_fun = likfuninvbat, weights, fixed_mu = NA, f
   # If kp or lam is fixed, we only need univariate optimization.
   if (!is.na(fixed_kp)) {
 
-    lam_hat <- optimize(f = function(lam) llfib(mu = mu_hat, kp = fixed_kp, lam = lam),
+    lam_hat <- stats::optimize(f = function(lam) llfib(mu = mu_hat, kp = fixed_kp, lam = lam),
                         lower = -1, upper = 1, maximum = TRUE)$maximum
     return(c(mu = mu_hat, kp = fixed_kp, lam = lam_hat))
 
   } else if (!is.na(fixed_lam)) {
 
-    kp_hat <- optimize(f = function(kp) llfib(mu = mu_hat, kp = kp, lam = fixed_lam),
+    kp_hat <- stats::optimize(f = function(kp) llfib(mu = mu_hat, kp = kp, lam = fixed_lam),
                        lower = 0, upper = kp_max, maximum = TRUE)$maximum
 
     return(c(mu = mu_hat, kp = kp_hat, lam = fixed_lam))
@@ -58,7 +58,7 @@ maxlikbat <- function(x, likfunbat_fun = likfuninvbat, weights, fixed_mu = NA, f
   } else {
 
     # Find maximum likelihood estimates for the both parameters.
-    om <- optim(fn = function(params) -llfib(mu = mu_hat, kp = params[1], lam = params[2]),
+    om <- stats::optim(fn = function(params) -llfib(mu = mu_hat, kp = params[1], lam = params[2]),
                 method = "Nelder-Mead", control = list(trace = 0, maxit = 5*max_its),
                 par = c(init_kp, init_lam))
 
