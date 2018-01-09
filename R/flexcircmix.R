4b34807b4177bf1a3663ae90270247c26c683971
Kees Mulder <keestimmulder@gmail.com>
2018-01-09 16:35:34 +0100

    Expand package documentation

--- R/flexcircmix.R
+++ R/flexcircmix.R
@@ -1,12 +1,14 @@
 #' Package 'flexcircmix'
 #'
-#' @name flexcircmix
 #'
-#'   Fit mixtures of flexible circular distributions. This package allows
-#'   fitting mixtures of peaked and flat-topped circular distributions from the
-#'   von Mises-based Batschelet-type family of distributions. Estimation is
-#'   performed through MCMC (Markov chain Monte Carlo) for the Bayesian model
-#'   and the EM (Expectation-Maximization) Algorithm for the frequentist model.
+#' Fit mixtures of flexible circular distributions. This package allows fitting
+#' mixtures of peaked and flat-topped circular distributions from the von
+#' Mises-based Batschelet-type family of distributions. Estimation is performed
+#' through MCMC (Markov chain Monte Carlo) for the Bayesian model and the EM
+#' (Expectation-Maximization) Algorithm for the frequentist model. Uncertainty
+#' quantification is provided by MCMC or the bootstrap.
+#'
+#' @name flexcircmix
 #'
 #' @docType package
 #' @importFrom stats sd median quantile
