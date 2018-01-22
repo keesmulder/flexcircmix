f4927e79fe439b251997ee47c28720b254bbcd23
Kees Mulder <keestimmulder@gmail.com>
2018-01-22 16:14:05 +0100

    Add tests for kappa proposals

--- tests/testthat/test_mcmc_batmix.R
+++ tests/testthat/test_mcmc_batmix.R
@@ -24,14 +24,38 @@ test_that("MCMC runs", {
 
 
 
-
-
-
 test_that("Kappa proposals work", {
 
   expect_equal(dgammaprop(3, 10), dchisq(3, 10))
   expect_error(rgammaprop(10, 10, 2), NA)
 
+  x <-  rinvbatmix(20)
+
+  # Test if all starting values can be weird.
+  weird_init <- matrix(c(NA, 0, NA, NA), ncol = 4, nrow = 3, byrow = TRUE)
+  expect_error(
+    sam_pow_weird <- mcmcBatscheletMixture(x, Q = 10,
+                                         init_pmat = weird_init,
+                                         n_comp = 3, verbose = 1,
+                                         kp_bw = 3, bat_type = 'power'),
+    NA
+  )
+  # Test if all starting values can be weird.
+  weird_init <- matrix(c(NA, 1000, NA, NA), ncol = 4, nrow = 3, byrow = TRUE)
+  expect_error(
+    sam_pow_weird <- mcmcBatscheletMixture(x, Q = 10,
+                                           init_pmat = weird_init,
+                                           n_comp = 3, verbose = 1,
+                                           kp_bw = 3, bat_type = 'power'),
+    NA
+  )
+
+
+  # curve(flexcircmix:::dgammaprop(x, 10, 1), 0, 20)
+  # curve(flexcircmix:::dgammaprop(x, 10, 2), 0, 20)
+  # curve(flexcircmix:::dgammaprop(x, 10, .1), 0, 20)
+
+  skip("")
 
   x <-  rinvbatmix(200)
   sam_pow_1 <- mcmcBatscheletMixture(x, Q = 1000, n_comp = 3, verbose = 1,
@@ -39,7 +63,7 @@ test_that("Kappa proposals work", {
   sam_pow_2 <- mcmcBatscheletMixture(x, Q = 1000, n_comp = 3, verbose = 1,
                                      kp_bw = 1, bat_type = 'power')
   sam_pow_3 <- mcmcBatscheletMixture(x, Q = 1000,  n_comp = 3, verbose = 1,
-                                     kp_bw = .2, bat_type = 'power')
+                                     kp_bw = .02, bat_type = 'power')
 
   cbind(sam_pow_1$acceptance_rates[, 1],
         sam_pow_2$acceptance_rates[, 1],
@@ -49,6 +73,9 @@ test_that("Kappa proposals work", {
                  sam_pow_2$acceptance_rates[, 1],
                  sam_pow_3$acceptance_rates[, 1]))
 
+  plot.ts(sam_pow_1$mcmc_sample[,4])
+  plot.ts(sam_pow_2$mcmc_sample[,4])
+  plot.ts(sam_pow_3$mcmc_sample[,4])
 })
 
 
