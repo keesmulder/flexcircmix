c32fcbea3917990aad903179a196807650ae4262
Kees Mulder <keestimmulder@gmail.com>
2018-12-25 12:48:34 +0100

    Fix aic and bic generics

--- R/generics.R
+++ R/generics.R
@@ -10,33 +10,33 @@ gapless_circ <- function(th) {
 
 #' Log Marginal Likelihood via Bridge Sampling
 #'
+#' Obtain the log of the Marginal Likelihood through Bridge sampling. This is
+#' useful to perform any Bayesian hypothesis test between any set of models.
 #'
-#'   More details are provided under
-#'   \code{\link[bridgesampling:bridge_sampler]{bridge_sampler}}.
+#' More details are provided under
+#' \code{\link[bridgesampling:bridge_sampler]{bridge_sampler}}.
 #'
-#' @param bm_mod A \code{batmixmod} object.
-#' @param ... Additional arguments to be passed to \code{bridge_sampler.matrix}
+#' @param samples A \code{batmixmod} object (from \code{\link{fitbatmix}}),
+#'   which contains the MCMC sample required for bridge sampling.
+#' @param ... Additional arguments to be passed to \code{bridge_sampler.matrix}.
 #'
-#' @seealso \code{
-#'   \link[brms:bayes_factor]{bayes_factor},
-#'   \link[brms:post_prob]{post_prob}
-#' }
-#'
+#' @seealso \code{ \link[brms:bayes_factor]{bayes_factor},
+#'   \link[brms:post_prob]{post_prob} }
 #'
 #' @method bridge_sampler batmixmod
 #' @importFrom bridgesampling bridge_sampler
 #' @export bridge_sampler
 #' @export
 #'
-bridge_sampler.batmixmod <- function(bm_mod, ...) {
+bridge_sampler.batmixmod <- function(samples, ...) {
 
-  if (bm_mod$method != "bayes") {
+  if (samples$method != "bayes") {
     stop("Bridge sampling is only possible for method =='bayes'.")
   }
 
-  n_comp <- bm_mod$n_components
+  n_comp <- samples$n_components
 
-  sam <- bm_mod$mcmc_sample[, 1:(4*n_comp)]
+  sam <- samples$mcmc_sample[, 1:(4*n_comp)]
 
   lb <- rep(c(-2*pi, 0, -1, 0), each = n_comp)
   ub <- rep(c(2*pi, Inf, 1, 1), each = n_comp)
@@ -44,35 +44,43 @@ bridge_sampler.batmixmod <- function(bm_mod, ...) {
   names(lb) <- colnames(sam)
   names(ub) <- colnames(sam)
 
-  bridgesampling::bridge_sampler(data = bm_mod$x,
+  bridgesampling::bridge_sampler(data = samples$x,
                                  samples = as.matrix(sam),
                                  param_types = rep(c("circular", "real", "real", "simplex"), each = n_comp),
-                                 log_posterior = bm_mod$log_posterior,
+                                 log_posterior = samples$log_posterior,
                                  lb = lb, ub = ub, ...)
 }
 
 
 #' Extract AIC from Batmixmod
 #'
-#' @param bm_mod A \code{batmixmod} object.
+#' @param object A \code{batmixmod} object.
 #' @param ... Ignored.
 #'
 #' @return The numerical value of the AIC.
+#'
+#' @method AIC batmixmod
+#' @importFrom stats AIC
+#' @export AIC
 #' @export
 #'
-AIC.batmixmod <- function(bm_mod, ...) {
-  bm_mod$ic_mat["aic", 2]
+AIC.batmixmod <- function(object, ...) {
+  object$ic_mat["aic", 2]
 }
+
 
 
 #' Extract BIC from Batmixmod
 #'
-#' @param bm_mod A \code{batmixmod} object.
+#' @param object A \code{batmixmod} object.
 #' @param ... Ignored.
 #'
 #' @return The numerical value of the BIC.
+#' @method BIC batmixmod
+#' @importFrom stats BIC
+#' @export BIC
 #' @export
 #'
-BIC.batmixmod <- function(bm_mod, ...) {
-  bm_mod$ic_mat["bic", 2]
+BIC.batmixmod <- function(object, ...) {
+  object$ic_mat["bic", 2]
 }
